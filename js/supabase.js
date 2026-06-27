537face591d89d5231e99fa94c063d0db9979c3d
abdhaal <abdhaal0@gmail.com>
2026-06-28 01:43:58 +0530

    Add alert for Supabase JS loading

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,10 @@
+alert("Supabase JS Loaded");
+
 const SUPABASE_URL = "https://jrjigvhzkicmgketrmbr.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
