a5f7c9c17cf08567ce235d36b6689b8500044703
abdhaal <abdhaal0@gmail.com>
2026-06-28 00:29:08 +0530

    Initialize Supabase client with URL and key

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://jrjigvhzkicmgketrmbr.supabase.co";
+const SUPABASE_KEY = "2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
+
+const supabase = window.supabase.createClient(
+  https://jrjigvhzkicmgketrmbr.supabase.co,
+  2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK
+);
