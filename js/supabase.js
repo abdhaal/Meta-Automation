d93e265c3d6c861049d2e94c9c7d88a7ae6d4fe1
abdhaal <abdhaal0@gmail.com>
2026-06-28 03:10:16 +0530

    Update Supabase key in supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ alert("Supabase JS Loaded");
 
 const SUPABASE_URL = "https://jrjigvhzkicmgketrmbr.supabase.co";
 
-const SUPABASE_KEY = "YOUR_FULL_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "sb_publishable_2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
 
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
