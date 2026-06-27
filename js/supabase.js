2dcf68cb39e649f662ba79a2227fc7351ef31834
abdhaal <abdhaal0@gmail.com>
2026-06-28 04:47:29 +0530

    Change Supabase key to JWT token
    Updated Supabase key to a new JWT token.

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://jrjigvhzkicmgketrmbr.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impyamlndmh6a2ljbWdrZXRybWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NzYyODEsImV4cCI6MjA5ODE1MjI4MX0.4FHwDGywcybt_tu52Dv5e2YEgCN3uKbKI0l844RA3Og";
 
 window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
