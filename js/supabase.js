c25192d34224813ff81f01da1695055d7ac8c010
abdhaal <abdhaal0@gmail.com>
2026-06-28 01:21:25 +0530

    Update Supabase key to use publishable key

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://jrjigvhzkicmgketrmbr.supabase.co";
-const SUPABASE_KEY = "2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
+
+const SUPABASE_KEY = "sb_publishable_2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK";
 
 const supabase = window.supabase.createClient(
-  https://jrjigvhzkicmgketrmbr.supabase.co,
-  2IokJkNZ8HO6n-pDMg0zLg_w7dkexlK
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
