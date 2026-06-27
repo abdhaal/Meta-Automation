9ac8d0592b0f113091161c1d8c7f8240d0ed4476
abdhaal <abdhaal0@gmail.com>
2026-06-28 02:02:08 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,9 @@
-alert(window.supabaseClient);
-
-const supabase = window.supabaseClient;
-
 alert("App JS Loaded");
 
 function facebookLogin() {
-    const username = prompt("Enter Facebook Username");
-    const password = prompt("Enter Facebook Password");
-
-    saveSocial("Facebook", username, password);
+    alert("Facebook button working");
 }
 
 function instagramLogin() {
-    const username = prompt("Enter Instagram Username");
-    const password = prompt("Enter Instagram Password");
-
-    saveSocial("Instagram", username, password);
-}
-
-async function saveSocial(platform, username, password) {
-
-    alert("saveSocial Started");
-
-    const { data, error } = await supabase
-        .from("social_accounts")
-        .insert([
-            {
-                platform: platform,
-                username: username,
-                password: password
-            }
-        ]);
-
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
-
-    if (error) {
-        alert("Supabase Error: " + error.message);
-    } else {
-        alert("Data Saved Successfully");
-    }
+    alert("Instagram button working");
 }
